0b4c4c370bd013ca2166bfd9adf193d1a2210c26
yurii macbook pro <yurikorock777@gmail.com>
2025-11-01 08:40:49 +0200

    feat: add validate to registerfom

---
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -0,0 +1,59 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import css from "./RegistrationForm.module.css";
+
+const schema = yup
+  .object({
+    name: yup.string().required("Name is required"),
+    email: yup
+      .string()
+      .matches(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/, "Invalid email format")
+      .required("Email is required"),
+    password: yup
+      .string()
+      .matches(
+        /^(?=(?:.*[A-Za-z]){6,})(?=.*\d)[A-Za-z\d]{7,}$/,
+        "Password must contain at least 6 letters and 1 number"
+      )
+      .min(8, "Password must be at least 8 characters")
+      .max(50, "Password must be at most 50 characters")
+      .required("Password is required"),
+  })
+  .required();
+
+interface FormData {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export default function RegistrationForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+  const onSubmit = (data: FormData) => console.log(data);
+
+  return (
+    <div className={css.wrapper}>
+      {/* /* ** FORM ** */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" placeholder="Name" {...register("name")} />
+        {errors.name && <p>{errors.name.message}</p>}
+        <input type="email" placeholder="Email" {...register("email")} />
+        {errors.email && <p>{errors.email.message}</p>}
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+        />
+        {errors.password && <p>{errors.password.message}</p>}
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+}
--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,11 +1,13 @@
 import Header from "../../components/Header/Header"
 import Hero from "../../components/Hero/Hero";
+import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
 
 export default function RegisterPage() {
   return (
     <>
     <Header/>
     <Hero/>
+    <RegistrationForm/>
       <p>Register Page</p>
     </>
   );
