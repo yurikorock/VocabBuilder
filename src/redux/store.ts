70d5a875ae183c0b45ee963e8d78d9de4ff0e30a
yurii macbook pro <yurikorock777@gmail.com>
2025-11-02 17:44:47 +0200

    feat: add persist-redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,37 @@
 //store.ts
 
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import authReducer from "./auth/authSlice";
 
-export const store = configureStore({ reducer: { auth: authReducer } });
+import storage from "redux-persist/lib/storage";
+
+// Persisting token field from auth slice to localstorage
+const authPersistConfig = {
+  key: "auth",
+  storage,
+  whitelist: ["token"], // зберігаємо лише токен
+};
+
+export const store = configureStore({
+  reducer: { auth: persistReducer(authPersistConfig, authReducer) },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+export const persistor = persistStore(store);
