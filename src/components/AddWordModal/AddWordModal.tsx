9dd918acc09b2f1e9445190c8396e8964a4e4808
yurii macbook pro <yurikorock777@gmail.com>
2025-11-16 10:25:50 +0200

    style: add style to select

--- src/components/AddWordModal/AddWordModal.tsx
+++ src/components/AddWordModal/AddWordModal.tsx
@@ -1,14 +1,82 @@
-import type { JSX } from "react";
+import { useEffect, useState, type JSX } from "react";
+import Select from "react-select";
+import { useAppDispatch, useAppSelector } from "../../redux/store";
+import { selectCategories } from "../../redux/words/selectors";
+import { fetchWordsCategories } from "../../redux/words/operation";
 
-import css from "./AddWordModal.module.css"
+import css from "./AddWordModal.module.css";
 
+export default function AddWordModal(): JSX.Element {
+  const dispath = useAppDispatch();
+  const [selectedCategory, setSelectedCategory] = useState<{
+    value: string;
+    label: string;
+  } | null>(null);
 
+  //Categories Redux
+  const categories = useAppSelector(selectCategories);
 
+  // ✅ Отримуємо категорії з бекенда при першому рендері
 
-export default function AddWordModal():JSX.Element {
+  useEffect(() => {
+    dispath(fetchWordsCategories());
+  }, [dispath]);
+
+   //перетворюємо масив  у потрібний для react-select формат.
+  const options = categories?.map((cat: string) => ({
+    value: cat,
+    label: cat[0].toUpperCase() + cat.slice(1),
+  }));
+
+  const whatIsCategorySelected = selectedCategory?.value;
+
   return (
-    <div className={css.container}>
-     <h4>AddWordModal</h4>
+    <div className={css.addwords_modal_container}>
+      <div className={css.title_wrap}>
+        <h3 className={css.title}>Add word</h3>
+        <p className={css.descr}>
+          Adding a new word to the dictionary is an important step in enriching
+          the language base and expanding the vocabulary.
+        </p>
+      </div>
+      <form>
+        <Select
+          options={options}
+          placeholder="Categories"
+          name="categories"
+          unstyled //обнулили стилі
+          isSearchable={false} // вимикаємо інпут повністю
+          value={selectedCategory}
+          // onChange={(opt) => dispatch(setLevel(opt?.value || ""))}
+          onChange={(option) => setSelectedCategory(option)}
+          classNamePrefix="custom-select"
+        />
+
+        <div
+          className={css.block_radio_verbs}
+          style={{
+            visibility:
+              whatIsCategorySelected === "verb" ? "visible" : "hidden",
+          }}
+        >
+          <label className={css.checkbox_label}>
+            <input
+              type="radio"
+              name="verbType"
+              className={css.checkbox_verbs}
+            />
+            Regular
+          </label>
+          <label className={css.checkbox_label}>
+            <input
+              type="radio"
+              name="verbType"
+              className={css.checkbox_verbs}
+            />
+            Irregular
+          </label>
+        </div>
+      </form>
     </div>
   );
 }
